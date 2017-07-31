0232dfee0ed8a3a3d5836779e94834dbae1d23ea
Nikoloz Tsimakuridze <n.tsimakuridze@freeuni.edu.ge>
2017-07-31 15:46:05 +0200

    Moved hypergraph functions to a package and changed code accordingly

---
+++ HypergraphFunctions.m
@@ -0,0 +1,63 @@
+(* ::Package:: *)
+
+BeginPackage["HypergraphFunctions`"];
+
+unsortedOverlay[h1_,h2_]:=Complement[Union[h1,h2],Intersection[h1,h2]];
+overlay[h1_,h2_]:=Sort[unsortedOverlay[h1,h2]];
+overlay[h1_,h2_,ho__]:=overlay[overlay[h1,h2],ho];
+
+delete[h_,qubit_]:=Select[h,!MemberQ[#,qubit]&];
+projection[h_,qubit_]:=DeleteCases[#,qubit]& /@ Select[h,MemberQ[#,qubit]&];
+unsortedShrink[h_,qubit_]:=unsortedOverlay[delete[h,qubit],projection[h,qubit]];
+shrink[h_,qubit_]:=overlay[delete[h,qubit],projection[h,qubit]];
+
+compactify[h_,qubit_,numQubits_]:=h/.Table[i->(i-1),{i,qubit+1,numQubits}];
+
+compactDelete[h_,qubit_,numQubits_]:=compactify[delete[h,qubit],qubit,numQubits];
+compactProjection[h_,qubit_,numQubits_]:=compactify[projection[h,qubit],qubit,numQubits];
+compactShrink[h_,qubit_,numQubits_]:=compactify[shrink[h,qubit],qubit,numQubits];
+
+memThreshold=7;
+memWeight[h_,numQubits_]:=memWeight[h,numQubits]=If[numQubits==0,
+	1 - 2 Boole[MemberQ[h,{}]],
+	With[{d=delete[h,numQubits],s=unsortedShrink[h,numQubits]},
+		memWeight[d,numQubits-1]+memWeight[s,numQubits-1]
+	]
+];
+
+weight[h_,numQubits_]:=If[numQubits<=memThreshold,
+	memWeight[h,numQubits],
+	With[{d=delete[h,numQubits],s=unsortedShrink[h,numQubits]},
+		weight[d,numQubits-1]+weight[s,numQubits-1]
+	]
+];
+
+isOrthogonal[h_,numQubits_]:=weight[h,numQubits]==0;
+areOrthogonal[h1_,h2_,numQubits_]:=isOrthogonal[overlay[h1,h2],numQubits];
+
+degree[h_,i_]:=Length[projection[h,i]];
+degreeSequence[h_,numQubits_]:=Table[degree[h,i],{i,numQubits}];
+
+PermuteVertices[h_,permutation_,numQubits_]:=Sort[Sort/@(h/.Table[k->permutation[[k]],{k,numQubits}])];
+removePermutations[hList_,numQubits_]:=Module[{permutations=Permutations[Range[numQubits]], alreadyFound,uniqueData,i},
+	Clear[alreadyFound];
+	For[i=1,i<=Length[hList],i++,
+		alreadyFound[hList[[i]]]=False;
+	];
+	uniqueData=Reap[
+		For[i=1,i<=Length[hList],i++,
+			If[!alreadyFound[hList[[i]]],
+				Sow[hList[[i]]];
+				For[j=1,j<=Length[permutations],j++,
+					alreadyFound[PermuteVertices[hList[[i]],permutations[[j]],numQubits]]=True;
+				];
+			];
+		];
+	][[2]];
+	If[Length[uniqueData]>0,
+		uniqueData[[1]],
+		{}
+	]
+];
+
+EndPackage[];
